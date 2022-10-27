353373abbf12d473917a33fa7f728ab5bc01fe4c
asadaman42 <asadaman42@gmail.com>
2022-10-27 17:35:44 +0600

    photo added

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const category = require('./data/category.json');
+
 app.get('/', (req, res) => {
-    res.send('seesrver is running');
+    res.send('seeeerver is running');
 });
 
-app.listen(port, ()=>{
+app.get('/category', (req, res) => {
+    res.send(category);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const course = category.filter(item => item.id === id);
+    res.send(course);   
+});
+
+app.listen(port, () => {
     console.log(`server is running on port: ${port}`);
 });
