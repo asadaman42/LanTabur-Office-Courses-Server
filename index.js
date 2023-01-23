6d22d1d9117912ff566007104b67ebf8fdcc5116
Md Asadullah <108480969+asadaman42@users.noreply.github.com>
2023-01-23 19:34:35 +0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 const category = require('./data/category.json');
 
 app.get('/', (req, res) => {
-    res.send('seeeerver is running');
+    res.send('LanTabur Office Course Server is running');
 });
 
 app.get('/category', (req, res) => {
